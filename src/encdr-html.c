3e312e5593745cc3f9e0f43719d16c6ad84330c4
Stephen R. Kifer <srk@kigra.com>
2026-02-06 20:06:14 -0500

    html encoder planning

--- src/encdr-html.c
+++ src/encdr-html.c
@@ -16,8 +16,7 @@
   General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with GNU Emacs.  If not, see
-  <https://www.gnu.org/licenses/>.
+  along with Situsest.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "situsest.h"
@@ -28,5 +27,28 @@ encode_html(struct file_list_node *fnode)
 {
 	printf("%s - %s\n", fnode->fname, fnode->lname);
 	//printf("-- %s \n", fnode->lname);
+	/*
+	  algorithm thoughts
+
+	  This encodes a single html file.
+	  Initial plan is just to copy html files with no change or processing.
+	  Should overwrite be default?
+
+	  Check for overwrite or sync flags
+	  If overwrite
+	    Copy over file overwriting any file that already exists
+	  If sync
+	    Check if dest file exists
+	      If exists
+	        Compare files
+		  If different overwrite dest file with new source
+		  Else skip file
+	      Else copy source file
+	 Else ?? any other mode ??
+
+	 How do I clean up files that exist in dest but no longer in source?
+	 Opt 1: Gen a list of every file in dest; remove from list as file processed; delete any leftovers
+	 Opt 2: Gen a list of files as processed; use list to parse dest, deleting any not on list
+	 Opt 3: ?
 }
 
